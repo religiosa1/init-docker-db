52a490a073f20b441f21655012ee01d7d10c511a
Viacheslav Azarov <public@religiosa.ru>
2025-02-02 23:55:27 +0100

    random container name generator

--- RandomName/Generate.go
+++ RandomName/Generate.go
@@ -1,5 +1,17 @@
 package RandomName
 
+import (
+	"fmt"
+	"math/rand"
+)
+
+/* List of tokens (adjectives and nouns) borrowed from the boring-name-generator
+ * npm package.
+ * https://github.com/boringprotocol/boring-name-generator
+ */
+
 func Generate() string {
-	return "TODO-RANDOM"
+	adjIdx := rand.Intn(len(adjectives))
+	nounIdx := rand.Intn(len(nouns))
+	return fmt.Sprintf("%s-%s", adjectives[adjIdx], nouns[nounIdx])
 }
